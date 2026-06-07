edf92cbfa047b053566007ff720020fbd6742e75
DhruthiRao1 <gadhru1@gmail.com>
2026-06-07 23:37:18 +0530

    Update retriever.py

--- agents/retriever.py
+++ agents/retriever.py
@@ -1,2 +1,12 @@
 def retrieve(ticket):
-    return "Knowledge Base Article Retrieved"
+
+    if "vpn" in ticket.lower():
+        return "VPN troubleshooting knowledge article"
+
+    if "outlook" in ticket.lower():
+        return "Outlook repair knowledge article"
+
+    if "password" in ticket.lower():
+        return "Password reset procedure"
+
+    return "General IT support knowledge article"
